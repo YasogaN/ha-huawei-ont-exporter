f3ba8cd529d2c8a0faff81bc39f34c288bb2a680
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2026-08-09 21:14:42 +0530

    refactor: run scripts with busybox /bin/sh instead of bash

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM alpine:3.21
 
 RUN apk add --no-cache \
-    bash \
     openssh-client \
     sshpass
 
--- scripts/main.sh
+++ scripts/main.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # ==============================================================================
 # Huawei ONT bandwidth exporter for Home Assistant
